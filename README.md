094ee9bf794286873405d300c500e6d54f53d068
Kemmegni Lucresse <lucressekemmegni@gmail.com>
2025-09-03 00:08:29 +0100

    Ajout du fichier README.md

---
+++ README.md
@@ -0,0 +1,19 @@
+# FullStackDev Backend
+
+## Description
+
+Ce projet est le backend d'une application full-stack, construit avec Node.js. Il fournit les API nécessaires pour gérer les fonctionnalités de l'application, telles que la gestion des utilisateurs, les requêtes de données, et d'autres services backend. Le backend est déployé sur un service cloud (par exemple, Fly.io ou Render) pour assurer une disponibilité en ligne.
+
+## Prérequis
+
+- Node.js (version 18.x ou supérieure)
+- npm (version 9.x ou supérieure)
+- Un compte sur un service cloud (Fly.io ou Render) pour le déploiement
+
+## Installation
+
+1. Clonez ce dépôt :
+   ```bash
+   git clone <'https://phonebackend-dbk3.onrender.com'>
+   cd FullStackDev_Backend
+   ```
--- index.js
+++ index.js
@@ -1,130 +1,130 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express')
+const morgan = require('morgan')
 
-const app = express();
+const app = express()
 
-const { StatusCodes } = require("http-status-codes");
+const { StatusCodes } = require('http-status-codes')
 
-app.use(express.json());
+app.use(express.json())
 
-app.use(morgan("tiny"));
+app.use(morgan('tiny'))
 
 let persons = [
   {
     id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
+    name: 'Arto Hellas',
+    number: '040-123456',
   },
   {
     id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
   },
   {
     id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
+    name: 'Dan Abramov',
+    number: '12-43-234345',
   },
   {
     id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
   },
-];
+]
 
 //configuration du midleware morgan pour pouvoir renvoyer les donnes du body dan sla console
 
-morgan.token("postToken", (req) => {
-  if (req.method === "POST") {
-    return JSON.stringify(req.body);
+morgan.token('postToken', (req) => {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body)
   }
-  return "-";
-});
+  return '-'
+})
 
 //utilisation du token dans le format morgan
 
 app.use(
   morgan(
-    ":method :url :status :res[content-length] - :response-time ms :postToken"
+    ':method :url :status :res[content-length] - :response-time ms :postToken'
   )
-);
+)
 
-app.get("/api/persons", (request, response) => {
-  response.json(persons);
-});
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find((person) => person.id === id)
 
-  if (person) return response.json(person);
+  if (person) return response.json(person)
 
-  response.status(StatusCodes.NOT_FOUND).end();
-});
+  response.status(StatusCodes.NOT_FOUND).end()
+})
 
-app.get("/info", (request, response) => {
-  const time = new Date();
-  const nbrePeople = persons.length;
+app.get('/info', (request, response) => {
+  const time = new Date()
+  const nbrePeople = persons.length
   const result = `<p>PhoneBook has info for ${nbrePeople} people</p>
-   <p>${time}</p>`;
+   <p>${time}</p>`
 
-  response.send(result);
-});
+  response.send(result)
+})
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.filter((person) => person.id !== id);
-  if (person) return response.json(person);
-  response.status(StatusCodes.NO_CONTENT).end();
-});
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.filter((person) => person.id !== id)
+  if (person) return response.json(person)
+  response.status(StatusCodes.NO_CONTENT).end()
+})
 
 const generateID = (persons, min = 1, max = 1000000) => {
-  const existsId = new Set(persons.map((pers) => pers.id));
-  let id;
+  const existsId = new Set(persons.map((pers) => pers.id))
+  let id
 
   do {
-    id = Math.floor(Math.random() * (max - min + 1)) + min;
-  } while (existsId.has(id));
+    id = Math.floor(Math.random() * (max - min + 1)) + min
+  } while (existsId.has(id))
 
-  return id;
-};
+  return id
+}
 
-app.post("/api/persons", (request, response) => {
-  console.log("hello");
-  const body = request.body;
+app.post('/api/persons', (request, response) => {
+  console.log('hello')
+  const body = request.body
 
-  const exitsName = persons.some((person) => person.name === body.name);
+  const exitsName = persons.some((person) => person.name === body.name)
   //verifier si le numero exits deja
 
   if (!body.name || !body.number)
-    return response.status(400).json({ error: "name or number missing" });
+    return response.status(400).json({ error: 'name or number missing' })
 
   //verifier si un non exists deja
 
   if (exitsName)
     return response
       .status(400)
-      .json({ error: `${body.name} already exists in the phonebook` });
+      .json({ error: `${body.name} already exists in the phonebook` })
 
   const newPerson = {
     name: body.name,
     number: body.number,
     id: generateID(persons),
-  };
+  }
 
-  persons = persons.concat(newPerson);
-  response.status(201).json(newPerson);
-});
+  persons = persons.concat(newPerson)
+  response.status(201).json(newPerson)
+})
 
 //midleware pour gerer les routes inexistantes
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3002
 app.listen(PORT, () => {
-  console.log(`server runing on port ${PORT}`);
-});
+  console.log(`server runing on port ${PORT}`)
+})
